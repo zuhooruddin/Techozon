243a2f2afe1f6f6ab052ef4505d9f547ebd5dcb6
zuhooruddin <100421830+zuhooruddin@users.noreply.github.com>
2024-06-02 15:27:28 +0500

    Update page.js

--- app/posts/page.js
+++ app/posts/page.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Link from 'next/link';
 
 const fetchPosts = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`);
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
 
   const post = await response.json();
   console.log("Postsss",post)
